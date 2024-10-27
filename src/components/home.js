518d97a05cd30ee29b923916f151d989e9727a59
narendra-varma-01 <paileenarendrasmy@gmail.com>
2024-10-27 12:27:30 +0530

    initial commit

---
+++ src/components/home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container vh-100">
+      <div className="row h-100 ">
+        <div className="col-12 col-md-6 mx-auto">
+          <div className="d-flex flex-column justify-content-center h-100">
+            <div className="text-center">
+              <div className="card text-center mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">###</h5>
+                  <p className="card-text">###</p>
+                  <a href="#" class="btn bg-info bg-gradient">Register</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
